9a2090ab3f8ead797bbee984a53279fd64b45ca1
ConHats <85992851+ConHats@users.noreply.github.com>
2024-11-15 19:33:49 +0200

    Update 7.c

--- Procedural Programming (C)/C/3/7.c
+++ Procedural Programming (C)/C/3/7.c
@@ -6,15 +6,13 @@ main(){
     int I,J, K, Number;
     K = 5;
     I = -2;
-    while (I<=K)
-    {
+    while (I<=K) {
         I = I + 2;
         K =  K - 1;
         printf("%2d\n", I+K);
     }
     Number = 4;
-    while (Number>=0)
-    {
+    while (Number>=0) {
         Number = Number - 1;
         printf("%1d", Number);
         printf("\n");
