fc2e6885367aa4748db83ee4b17bc0c8e7fb753f
ConHats <85992851+ConHats@users.noreply.github.com>
2024-11-15 19:27:10 +0200

    Update 1b.c

--- Procedural Programming (C)/C/3/1b.c
+++ Procedural Programming (C)/C/3/1b.c
@@ -12,11 +12,10 @@ main(){
     printf("Enter the limit: ");
     Limit = GetLong();
     Sum_of_Cars = count_of_Cars;
-    while(Sum_of_Cars<=Limit)
-        {
-            Sum_of_Cars = ceil(Sum_of_Cars*(1+rate));
-            Eth++;
-        }
+    while(Sum_of_Cars<=Limit) {
+        Sum_of_Cars = ceil(Sum_of_Cars*(1+rate));
+        Eth++;
+    }
     printf("%ld\n%ld\n", Eth, Sum_of_Cars);
     return 0;
 }
