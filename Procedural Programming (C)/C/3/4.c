e77b7a14c114679b5e256c41e00a7ab927ec422d
ConHats <85992851+ConHats@users.noreply.github.com>
2024-11-15 19:32:22 +0200

    Update 4.c

--- Procedural Programming (C)/C/3/4.c
+++ Procedural Programming (C)/C/3/4.c
@@ -8,39 +8,33 @@ main(){
     long total, outcome, sum = 0;
     printf("Enter password: ");
     code = GetInteger();
-    while (code != 1 && code != 2)
-        {
+    while (code != 1 && code != 2) {
+        printf("Wrong password.\n");
+        printf("Enter password: ");
+        code = GetInteger();
+    }
+    while (code==1) {
+        printf("Enter the number of people: ");
+        people = GetInteger();
+        if (people <= 10) {
+            total = people * 350;
+        }
+        else{
+            total = ceil(people * 350 - (people * 0.1 * 350));
+        }
+        sum += total;
+        printf("Price of vacancy: %ld\n", total);
+        printf("Enter password: ");
+        code = GetInteger();
+        while (code != 1 && code != 2) {
             printf("Wrong password.\n");
             printf("Enter password: ");
             code = GetInteger();
         }
-    while (code==1)
-        {
-            printf("Enter the number of people: ");
-            people = GetInteger();
-            if (people <= 10)
-                {
-                    total = people * 350;
-                }
-            else
-                {
-                    total = ceil(people * 350 - (people * 0.1 * 350));
-                }
-            sum += total;
-            printf("Price of vacancy: %ld\n", total);
-            printf("Enter password: ");
-            code = GetInteger();
-            while (code != 1 && code != 2)
-                {
-                    printf("Wrong password.\n");
-            printf("Enter password: ");
-                    code = GetInteger();
-                }
-        }
-    if (code==2)
-        {
-            outcome = sum - 20000;
-            printf("Profit or loss (-) for the trip: %ld", outcome);
-        }
+    }
+    if (code==2) {
+        outcome = sum - 20000;
+        printf("Profit or loss (-) for the trip: %ld", outcome);
+    }
     return 0;
 }
