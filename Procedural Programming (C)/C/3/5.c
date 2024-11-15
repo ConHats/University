d126f26ab49759292eb37dfaa31260a139707cc3
ConHats <85992851+ConHats@users.noreply.github.com>
2024-11-15 19:33:03 +0200

    Update 5.c

--- Procedural Programming (C)/C/3/5.c
+++ Procedural Programming (C)/C/3/5.c
@@ -6,26 +6,24 @@ main(){
     int code, gender, weight, height, age, N, i, maxage = 0, maxcode, maxweight, maxheight;
     printf("N: ");
     N = GetInteger();
-    for (i=1; i<=N; i++)
-        {
-            printf("Enter code: ");
-            code = GetInteger();
-            printf("Enter gender(0 for male, 1 for female): ");
-            gender = GetInteger();
-            printf("Enter weight: ");
-            weight = GetInteger();
-            printf("Enter height: ");
-            height = GetInteger();
-            printf("Enter age: ");
-            age = GetInteger();
-                if (gender == 0 && maxage < age)
-                    {
-                        maxage = age;
-                        maxcode = code;
-                        maxweight = weight;
-                        maxheight = height;
-                    }
+    for (i=1; i<=N; i++) {
+        printf("Enter code: ");
+        code = GetInteger();
+        printf("Enter gender(0 for male, 1 for female): ");
+        gender = GetInteger();
+        printf("Enter weight: ");
+        weight = GetInteger();
+        printf("Enter height: ");
+        height = GetInteger();
+        printf("Enter age: ");
+        age = GetInteger();
+        if (gender == 0 && maxage < age) {
+            maxage = age;
+            maxcode = code;
+            maxweight = weight;
+            maxheight = height;
         }
+    }
     printf("%d_%d_%d_%d_%d", maxcode, gender, maxweight, maxheight, maxage);
     return 0;
 }
