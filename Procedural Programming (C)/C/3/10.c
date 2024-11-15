873984a151f58b24f706882880e2a856cbbcab34
ConHats <85992851+ConHats@users.noreply.github.com>
2024-11-15 19:20:51 +0200

    Update 10.c

--- Procedural Programming (C)/C/3/10.c
+++ Procedural Programming (C)/C/3/10.c
@@ -4,13 +4,11 @@
 
 main(){
     int i, j, k;
-    for (i=1; i<=10; i++)
-    {
-         for (j=1; j<=10; j++)
-            {
-                 printf("%4d", i*j);
-            }
-            printf("\n");
+    for (i=1; i<=10; i++) {
+        for (j=1; j<=10; j++) {
+            printf("%4d", i*j);
+        }
+        printf("\n");
     }
     return 0;
 }
