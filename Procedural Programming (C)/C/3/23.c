879033260d9ddda1da8b58432587022a5e6a6f61
ConHats <85992851+ConHats@users.noreply.github.com>
2024-11-15 19:29:18 +0200

    Update 23.c

--- Procedural Programming (C)/C/3/23.c
+++ Procedural Programming (C)/C/3/23.c
@@ -6,22 +6,17 @@ int main(){
     int number, i;
     printf("Enter number: ");
     number = GetInteger();
-    if (number%2==0)
-        {
-            i=0;
-            while (i<=number)
-            {
-                printf("%d ", i);
-                i=i+2;
-            }
-
+    if (number%2==0) {
+        i=0;
+        while (i<=number) {
+            printf("%d ", i);
+            i=i+2;
         }
-    else
-        {
-            for (i=1; i<=number; i+=2)
-            {
-                printf("%d ", i);
-            }
+    }
+    else {
+        for (i=1; i<=number; i+=2) {
+            printf("%d ", i);
         }
+    }
     return 0;
 }
