b61a6e2055a85e3789f6abe680c687735efaf10d
ConHats <85992851+ConHats@users.noreply.github.com>
2024-11-15 19:24:08 +0200

    Update 16.c

--- Procedural Programming (C)/C/3/16.c
+++ Procedural Programming (C)/C/3/16.c
@@ -5,23 +5,19 @@
 main(){
     double grade, sum=0, average, max1=0, max2=0;
     int i;
-    for (i=1; i<=100; i++)
-        {
-            printf("enter grade %d ->", i);
-            grade = GetReal();
-            sum += grade;
-            if (max1<grade)
-                {
-                    max1=grade;
-                }
-            if (max2<grade && grade<max1)
-                {
-                    max2=grade;
-                }
-
+    for (i=1; i<=100; i++) {
+        printf("enter grade %d ->", i);
+        grade = GetReal();
+        sum += grade;
+        if (max1<grade) {
+            max1=grade;
         }
+        if (max2<grade && grade<max1) {
+            max2=grade;
+        }
+    }
     average = sum/100;
-    printf("Avarage= %.2f\n", average);
+    printf("Average= %.2f\n", average);
     printf("MAX1= %.2f\n", max1);
     printf("MAX2= %.2f", max2);
     return 0;
