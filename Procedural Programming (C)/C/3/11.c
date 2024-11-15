cb2de668825fffc549ce09c45b5330b70eb7a781
ConHats <85992851+ConHats@users.noreply.github.com>
2024-11-15 19:21:30 +0200

    Update 11.c

--- Procedural Programming (C)/C/3/11.c
+++ Procedural Programming (C)/C/3/11.c
@@ -4,12 +4,10 @@
 
 main(){
     int i, j;
-    for (i=1; i<=9; i++)
-    {
-        for (j=1; j<=9; j+=2)
-            {
+    for (i=1; i<=9; i++) {
+        for (j=1; j<=9; j+=2) {
                 printf("*");
-            }
+        }
         printf("*\n");
     }
     return 0;
