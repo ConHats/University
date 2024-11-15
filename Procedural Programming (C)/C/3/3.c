1348f7c49061d9ebafc8c4d49e8f7049d9f90378
ConHats <85992851+ConHats@users.noreply.github.com>
2024-11-15 19:30:29 +0200

    Update 3.c

--- Procedural Programming (C)/C/3/3.c
+++ Procedural Programming (C)/C/3/3.c
@@ -6,18 +6,17 @@ main(){
     long domestic_boxes, foreign_boxes, domestic_amount, foreign_amount, total_domestic = 0, total_foreign = 0;
     printf("Number of domestic boxes? ");
     domestic_boxes = GetLong();
-    while (domestic_boxes != -9999)
-        {
-            printf("Number of foreign boxes? ");
-            foreign_boxes = GetLong();
-            domestic_amount = domestic_boxes * 3500;
-            total_domestic += domestic_amount;
-            foreign_amount = foreign_boxes * 5000;
-            total_foreign += foreign_amount;
-            printf("%10ld%10ld%10ld\n", domestic_amount, foreign_amount, domestic_amount + foreign_amount);
-            printf("Number of domestic boxes? ");
-            domestic_boxes = GetLong();
-        }
+    while (domestic_boxes != -9999) {
+        printf("Number of foreign boxes? ");
+        foreign_boxes = GetLong();
+        domestic_amount = domestic_boxes * 3500;
+        total_domestic += domestic_amount;
+        foreign_amount = foreign_boxes * 5000;
+        total_foreign += foreign_amount;
+        printf("%10ld%10ld%10ld\n", domestic_amount, foreign_amount, domestic_amount + foreign_amount);
+        printf("Number of domestic boxes? ");
+        domestic_boxes = GetLong();
+    }
     printf("%10ld%10ld%10ld", total_domestic, total_foreign, total_domestic + total_foreign);
     return 0;
 }
