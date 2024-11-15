790f9140031a1aa49150ec62545f0c23bc0edd60
ConHats <85992851+ConHats@users.noreply.github.com>
2024-11-15 19:27:49 +0200

    Update 20.c

--- Procedural Programming (C)/C/3/20.c
+++ Procedural Programming (C)/C/3/20.c
@@ -6,19 +6,16 @@ main(){
     int age, max = 0, min = 100;
     printf("Enter age: ");
     age = GetInteger();
-    while (age != -1)
-        {
-            if (max < age)
-                {
-                    max = age;
-                }
-            if (min > age)
-                {
-                    min = age;
-                }
-            printf("Enter age: ");
-            age = GetInteger();
+    while (age != -1) {
+        if (max < age) {
+            max = age;
         }
+        if (min > age) {
+            min = age;
+        }
+        printf("Enter age: ");
+        age = GetInteger();
+    }
     printf("The oldest age is %d\n", max);
     printf("The youngest age is %d", min);
     return 0;
