d117f46d804b4a5985a55a8fdacd9c932aad11dd
ConHats <85992851+ConHats@users.noreply.github.com>
2024-11-15 19:24:28 +0200

    Update 17.c

--- Procedural Programming (C)/C/3/17.c
+++ Procedural Programming (C)/C/3/17.c
@@ -5,8 +5,7 @@
 
 main(){
     double x, fx;
-    for (x=0; x<=1; x+=0.05)
-    {
+    for (x=0; x<=1; x+=0.05) {
         fx=pow(x,4)-(5*pow(x,2))+3;
         printf("x=%f  fx=%f\n", x, fx);
     }
