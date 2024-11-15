2d942021fc50523f3e18f4f4e21d24731f5f4bfd
ConHats <85992851+ConHats@users.noreply.github.com>
2024-11-15 19:22:37 +0200

    Update 14.c

--- Procedural Programming (C)/C/3/14.c
+++ Procedural Programming (C)/C/3/14.c
@@ -7,17 +7,15 @@ main(){
     printf("Enter number:");
     n = GetInteger();
     N = n;
-    while (n/10!=0)
-        {
-            n /= 10;
-            i++;
-        }
-    for (j=1; j<=i; j++)
-        {
-            x=N%10;
-            N=N/10;
-            sum=sum+x;
-        }
+    while (n/10!=0) {
+        n /= 10;
+        i++;
+    }
+    for (j=1; j<=i; j++) {
+        x=N%10;
+        N=N/10;
+        sum=sum+x;
+    }
     printf("The sum of digits is %d", sum);
     return 0;
 }
