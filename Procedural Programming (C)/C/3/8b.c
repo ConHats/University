289ae846eafce80619c6251723bd519138754762
ConHats <85992851+ConHats@users.noreply.github.com>
2024-11-15 19:34:22 +0200

    Update 8b.c

--- Procedural Programming (C)/C/3/8b.c
+++ Procedural Programming (C)/C/3/8b.c
@@ -4,12 +4,9 @@
 
 main(){
     int I, J, K;
-    for (I=1; I<=3; I++)
-    {
-        for (J=1; J<=3; J++)
-        {
-            for (K=I; K<=J; K++)
-            {
+    for (I=1; I<=3; I++) {
+        for (J=1; J<=3; J++) {
+            for (K=I; K<=J; K++) {
                 printf("%1d%1d%1d \n",  I, J, K);
             }
         }
