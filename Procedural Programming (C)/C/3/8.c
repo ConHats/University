a39a6bd2185de042921cfa7fea0fdc191c4a528e
ConHats <85992851+ConHats@users.noreply.github.com>
2024-11-15 19:18:56 +0200

    Update 8.c

--- Procedural Programming (C)/C/3/8.c
+++ Procedural Programming (C)/C/3/8.c
@@ -6,9 +6,9 @@ main(){
     int I, K;
     K = 5;
     for (I=-2; I<=3; I++)
-        {
-            printf("%2d\n", I+K);
-            K=1;
-        }
+    {
+        printf("%2d\n", I+K);
+        K=1;
+    }
     return 0;
 }
