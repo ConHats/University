15b2bd11f9d8ce0d104da1d8c3428d83c537397e
ConHats <85992851+ConHats@users.noreply.github.com>
2024-11-15 19:28:14 +0200

    Update 21.c

--- Procedural Programming (C)/C/3/21.c
+++ Procedural Programming (C)/C/3/21.c
@@ -4,8 +4,7 @@
 
 main(){
     int i, sum=0;
-    for (i=1; i<=20; i++)
-    {
+    for (i=1; i<=20; i++) {
         sum=sum+i;
     }
     printf("The sum of numbers from 1 to 20 except for 5 and 15 is %d", sum-20);
