98ae3f1a67e9b81d82de22bdca9fbf8891550b9a
ConHats <85992851+ConHats@users.noreply.github.com>
2024-11-15 19:25:12 +0200

    Update 18.c

--- Procedural Programming (C)/C/3/18.c
+++ Procedural Programming (C)/C/3/18.c
@@ -4,21 +4,18 @@
 
 main(){
     int index=0, max=0, min=100000, sum=0, amount;
-    while (sum<100000)
-        {
-            printf("Enter amount: ");
-            amount = GetInteger();
-            sum += amount;
-            index += 1;
-            if (max<amount)
-                {
-                    max = amount;
-                }
-            if (min>amount)
-                {
-                    min = amount;
-                }
+    while (sum<100000) {
+        printf("Enter amount: ");
+        amount = GetInteger();
+        sum += amount;
+        index += 1;
+        if (max<amount) {
+            max = amount;
         }
+        if (min>amount) {
+            min = amount;
+        }
+    }
     printf("Number of people: %d\n", index);
     printf("Total amount: %d\n", sum);
     printf("Largest amount: %d  ", max);
